406c5c06416e1078fb9d39874bd3f3782c3f2c62
Zevihex <OfficialZevihex@gmail.com>
2025-02-27 19:28:19 -0600

    Initial Commit

---
+++ src/pages/MainScreen.js
@@ -0,0 +1,12 @@
+
+function MainScreen() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>React!!!</h1>
+      </header>
+    </div>
+  );
+}
+
+export default MainScreen;
